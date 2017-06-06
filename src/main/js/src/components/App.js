4fe14c5af53ed6a50acd83e20a38263d15727da7
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2017-06-06 09:08:27 +0300

    eject react app

--- src/main/js/src/components/App.js
+++ src/main/js/src/components/App.js
@@ -5,14 +5,12 @@ import Sidebar from './Sidebar'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
-import 'react-metismenu/dist/react-metismenu-standart.css'
 
 import 'bootstrap/dist/js/bootstrap.js'
 
 const App = () => (
   <div>
     <Sidebar />
-    <Navbar />
   </div>
 )
 
--- src/main/js/src/components/Sidebar/components/SidebarView.js
+++ src/main/js/src/components/Sidebar/components/SidebarView.js
@@ -1,34 +1,8 @@
 import React from 'react'
-import './SidebarView.css'
-import MetisMenu from 'react-metismenu';
-
-const content=[
-  {
-    icon: 'dashboard',
-    label: 'Menu 1',
-    to: 'menu-1',
-  },
-  {
-    icon: 'user',
-    label: 'Profile',
-    to: 'menu-2',
-  },
-  {
-    icon: 'bolt',
-    label: 'Menu 3',
-    content: [
-      {
-        icon: 'bolt',
-        label: 'Sub Menu',
-        to: 'sub-menu',
-      },
-    ],
-  },
-];
+import './SidebarView.scss'
 
 const SidebarView = () => (
     <div className="sidebar-wrapper">
-        <MetisMenu content={content} activeLinkFromLocation /> 
     </div>
 )
 
