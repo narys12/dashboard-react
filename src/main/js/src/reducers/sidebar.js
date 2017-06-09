0fb424d2b5196a84d071498c49447dbbae116a94
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2017-06-09 16:09:21 +0300

    perform reducer

--- src/main/js/src/reducers/sidebar.js
+++ src/main/js/src/reducers/sidebar.js
@@ -1,5 +1,18 @@
-export function sidebarVisibility() {
-    return {
-        value: false
+const initialState = {
+    visibility: true
+}
+
+export function sidebarVisibility(state = {}, action) {
+    if (typeof state === 'undefined') {
+        return initialState
+    }
+
+    switch (action.type) {
+        case 'SET_VISIBILITY' : 
+            return Object.assign({}, state, {
+                visibility: action.value
+            })
+        default: 
+            return state
     }
 }
