40438a4658cdcdf2ccf1246b7741e7d4c880476b
narys12 <naris.razafimahatratra@zoho.com>
2017-06-19 18:58:03 +0300

    restructure code

--- src/main/js/src/components/SidebarView/index.js
+++ src/main/js/src/components/SidebarView/index.js
@@ -3,23 +3,20 @@ import './style.scss'
 
 class SidebarView extends React.Component {
 
-    render() {
-        const isOpen = this.props.sidebarVisibility.value
-        const openSidebar = () => this.props.changeSidebarVisibility(true)
-        const closeSidebar = () => this.props.changeSidebarVisibility(false)
-        let sideBarWrapper = "sidebar-wrapper"
-        let icon = null;
-        if (!isOpen) {
-            sideBarWrapper = sideBarWrapper + " toggle-menu"
-            icon = <i className="fa fa-bars fa-lg" onClick={openSidebar}></i>
-        } else {
-            sideBarWrapper = sideBarWrapper.replace(" toggle-menu", "")
-            icon = <i className="fa fa-close fa-lg" onClick={closeSidebar}></i>
-        }
+    constructor(props) {
+        super(props)
+        this.openSidebar = () => this.props.changeSidebarVisibility(true)
+        this.closeSidebar = () => this.props.changeSidebarVisibility(false)
+    }
 
+    render() {
+        let isOpen = this.props.sidebarVisibility.value
         return (
-            <div className={sideBarWrapper}>
-                {icon}
+            <div className={isOpen ? "sidebar-wrapper" : "sidebar-wrapper toggle-menu"}>
+                <i 
+                    className={isOpen ? "fa fa-close fa-lg" : "fa fa-bars fa-lg"}
+                    onClick={isOpen ? this.closeSidebar : this.openSidebar}
+                ></i>
                 <div className="sidebar-header">
                     Your Site
                 </div>
