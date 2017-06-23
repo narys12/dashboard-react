18dff6f501ffa7e6612d1db2d83f54d979a6fc99
narys12 <naris.razafimahatratra@zoho.com>
2017-06-23 21:53:24 +0300

    use css to fix submenu position

--- src/main/js/src/components/SidebarView/CatalogView/index.js
+++ src/main/js/src/components/SidebarView/CatalogView/index.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class CatalogView extends Component {
     render() {
         return (
-            <span>
-                Catalog
-                <ul className={this.props.isParentClicked ? "display-submenu" : "hide-submenu"}>
-                    <li>Products</li>
-                </ul>
-            </span>
+            <ul
+                className={this.props.isParentClicked
+                ? "display-submenu"
+                : "hide-submenu"}>
+                <li>Products</li>
+                <li>Categories</li>
+            </ul>
         );
     }
 }
--- src/main/js/src/components/SidebarView/index.js
+++ src/main/js/src/components/SidebarView/index.js
@@ -8,8 +8,12 @@ class SidebarView extends React.Component {
 
     constructor(props) {
         super(props)
-        this.openSidebar = () => this.props.changeSidebarVisibility(true)
-        this.closeSidebar = () => this.props.changeSidebarVisibility(false)
+        this.openSidebar = () => this
+            .props
+            .changeSidebarVisibility(true)
+        this.closeSidebar = () => this
+            .props
+            .changeSidebarVisibility(false)
         this.state = {
             isCatalogClicked: false
         }
@@ -18,20 +22,32 @@ class SidebarView extends React.Component {
     render() {
         let isOpen = this.props.sidebarVisibility.value
         return (
-            <div className={isOpen ? "sidebar-wrapper" : "sidebar-wrapper toggle-menu"}>
-                <i 
-                    className={isOpen ? "fa fa-close fa-lg" : "fa fa-bars fa-lg"}
-                    onClick={isOpen ? this.closeSidebar : this.openSidebar}
-                ></i>
+            <div
+                className={isOpen
+                ? "sidebar-wrapper"
+                : "sidebar-wrapper toggle-menu"}>
+                <i
+                    className={isOpen
+                    ? "fa fa-close fa-lg"
+                    : "fa fa-bars fa-lg"}
+                    onClick={isOpen
+                    ? this.closeSidebar
+                    : this.openSidebar}></i>
                 <div className="sidebar-header">
                     Your Site
                 </div>
                 <ul className="sidebar-menu">
-                    <li className="sidebar-menu-item"><i className="fa fa-dashboard"></i><span>Dashboard</span></li>
-                    <li className="sidebar-menu-item" 
-                        onClick={() => this.setState((prevState) => ({isCatalogClicked: !prevState.isCatalogClicked}))}
-                    >
-                        <i className="fa fa-user"></i><CatalogView isParentClicked={this.state.isCatalogClicked}/>
+                    <li className="sidebar-menu-item">
+                        
+                            <i className="fa fa-dashboard"></i><span>Dashboard</span>
+                    </li>
+                    <li
+                        className="sidebar-menu-item"
+                        onClick={() => this.setState((prevState) => ({
+                        isCatalogClicked: !prevState.isCatalogClicked
+                    }))}>
+                        <i className="fa fa-user"></i><span>Catalog</span>
+                        <CatalogView isParentClicked={this.state.isCatalogClicked}/>
                     </li>
                 </ul>
             </div>
